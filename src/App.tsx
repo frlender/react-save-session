e92e3a3d77d22f0664b0309c930a1b7c0e83818a
Qiaonan Duan <geonann@gmail.com>
2023-07-26 17:41:42 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { SaveSession, ListSessions } from './comps/SaveSession';
+import { Dic } from './ExampleInterfaces';
+import { SessRecord } from './comps/Interfaces';
+import { useState, useEffect, ChangeEvent, useRef, FormEvent } from 'react'
+
+
 
 function App() {
+  const [gc,setGc] = useState(0)
+  const getData=()=>{
+    return {'a':5,'b':6}
+  }
+  const sessFormat=(sessions:SessRecord<Dic>[],remove:(x:string)=>void)=>{
+    return sessions.map((x)=><div key={x.uid}>
+        <span>{x.data.a}</span>
+        <span></span>
+      </div>)
+  }
+
+  const enterSess = (x:SessRecord<Dic>)=>{
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={()=>setGc(gc+1)}>update</button>
+      <ListSessions<Dic> dbName='test3'
+       enter={enterSess} gc={gc}></ListSessions>
+      <SaveSession<Dic> dbName='test3'
+        getData={getData} download></SaveSession>
     </div>
   );
 }
