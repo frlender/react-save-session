46426236c2328fe5edc18a5bc618fb5b33486ed8
Qiaonan Duan <geonann@gmail.com>
2023-07-27 13:10:40 +0800

    update

--- lib/SaveSession.d.ts
+++ lib/SaveSession.d.ts
@@ -1,6 +1,7 @@
 import { SaveSessionProps, ListSessionsProps, SessRecord } from "./Interfaces";
+import DBStore from "./DBStore";
 import 'react-edit-text/dist/index.css';
 declare function SaveSession<T>(props: SaveSessionProps<T>): import("react/jsx-runtime").JSX.Element;
 declare function ListSessions<T>(props: ListSessionsProps<T>): import("react/jsx-runtime").JSX.Element;
 declare function defaultSessFormat<T>(sessions: SessRecord<T>[], remove: (x: string) => void, enter: (x: SessRecord<T>) => void): import("react/jsx-runtime").JSX.Element[];
-export { SaveSession, ListSessions, defaultSessFormat };
+export { SaveSession, ListSessions, defaultSessFormat, DBStore };
